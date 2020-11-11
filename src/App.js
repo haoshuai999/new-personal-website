fb9b9fbd3a9216b1a53d73a12b5d8a5a9bfd6996
haoshuai999 <626593718@qq.com>
2020-11-11 00:20:13 -0500

    add three components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+import Todo from "./components/Todo";
 
-function App() {
-  const author = "Shuai";
+function App(props) {
+  const taskList = props.tasks.map(task => (
+    <Todo 
+      name={task.name} 
+      completed={task.completed} 
+      id={task.id}
+      key={task.id}
+     />
+  ));
+
+  const buttonList = props.buttons.map(btn => (
+    <FilterButton 
+      name = {btn.name}
+      pressed = {btn.pressed}
+      id = {btn.id}
+      key = {btn.id}
+    />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World!
-        </p>
-        <p>{author}'s Personal Website</p>
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        {buttonList}
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false }
+];
+
+const BUTTON = [
+  { id: "btn-0", name: "all", pressed: true },
+  { id: "btn-1", name: "Active", pressed: false },
+  { id: "btn-2", name: "Complete", pressed: false }
+]
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App tasks={DATA} buttons={BUTTON} />
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,4 +25,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
